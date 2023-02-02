842f63cb040839c25156371d30bf51da88076917
Mrunal Rawade <75116547+mrunal0502@users.noreply.github.com>
2023-02-02 12:39:43 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,22 @@
+let myArr=["camera","spain",4,true,null];
+
+//Array Methods
+console.log(myArr.length);
+//pop() removes the last element
+console.log(myArr.pop());
+console.log(myArr);
+//push() adds an element 
+myArr.push("harry");
+console.log(myArr);
+//shift() removes the first element
+myArr.shift()
+console.log(myArr);
+//unshift() shifts the specified element to the first position
+myArr.unshift('true');
+console.log(myArr);
+
+//toString
+console.log(myArr.toString());
+
+//sort()
+console.log(myArr.sort());
