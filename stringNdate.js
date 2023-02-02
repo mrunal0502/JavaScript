d43657b18fdeb8f4e808e4327a306e429b7f569a
Mrunal Rawade <75116547+mrunal0502@users.noreply.github.com>
2023-02-02 12:59:16 +0530

    Add files via upload

---
+++ stringNdate.js
@@ -0,0 +1,21 @@
+let str="Mrunal is doing well well";
+console.log(str.length);// returns length
+
+//indexOf
+console.log(str.indexOf('well'));
+
+console.log(str.lastIndexOf('well'));
+
+console.log(str.slice(2,6));
+
+m= str.replace("Mrunal","Siddhi");
+console.log(m);
+
+
+let myDate= new Date();
+console.log(myDate);
+console.log(myDate.getTime());
+console.log(myDate.getFullYear());
+console.log(myDate.getDay());
+console.log(myDate.getMonth());
+console.log(myDate.getHours());
